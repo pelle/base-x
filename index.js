d3cce8941c399d75c343db0757c196543bacff4e
Pelle Braendgaard <pelle@stakeventures.com>
2017-03-31 09:27:12 -0600

    fix really dumb typo

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ module.exports = function base (ALPHABET) {
     for (var k = 0; string[k] === LEADER && k < string.length - 1; ++k) {
       bytes.push(0)
     }
-    if (ByteArray.name === 'Uuint8array') {
+    if (ByteArray.name === 'Uint8array') {
       // This handles an issue in safari
       return new Uint8Array(bytes.reverse())
     }
